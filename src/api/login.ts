80fc5f28dfb2944c7540253f51cc4a9d7f564aa7
ourfor <ourfor@qq.com>
2024-03-19 21:37:55 +0800

    🥅 catch login error

--- src/api/login.ts
+++ src/api/login.ts
@@ -10,16 +10,21 @@ export async function login(username: string, password: string, endpoint: EmbyCo
     "X-Emby-Language": "zh-cn"
   }
   const url = makeEmbyUrl(params, `emby/Users/authenticatebyname`, endpoint)
-  const response = await fetch(url, {
-    "headers": {
-      "accept": "application/json",
-      "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
-      "content-type": "application/x-www-form-urlencoded; charset=UTF-8"
-    },
-    "referrerPolicy": "strict-origin-when-cross-origin",
-    "body": `Username=${username}&Pw=${password}`,
-    "method": "POST"
-  })
-  const data = await response.json() as User
-  return data
+  try {
+    const response = await fetch(url, {
+      "headers": {
+        "accept": "application/json",
+        "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+        "content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+      },
+      "referrerPolicy": "strict-origin-when-cross-origin",
+      "body": `Username=${username}&Pw=${password}`,
+      "method": "POST"
+    })
+    const data = await response.json() as User
+    return data
+  } catch (e) {
+    console.error(e)
+    return null
+  }
 }
