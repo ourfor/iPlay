6142947c49158329bb122c443c461c60a27c5261
ourfor <ourfor@qq.com>
2024-03-20 08:55:14 +0800

    ✨ add svg support

--- src/page/album/index.tsx
+++ src/page/album/index.tsx
@@ -1,11 +1,12 @@
-import { Api, Emby } from "@api/emby";
+import { Emby } from "@api/emby";
 import { PropsWithNavigation } from "@global";
 import { useAppSelector } from "@hook/store";
 import { EmbyResponse } from "@model/EmbyResponse";
 import { Media } from "@model/Media";
 import { MediaCard } from '@view/MediaCard';
+import { Spin } from "@view/Spin";
 import React, { useEffect, useState } from "react";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
 
 const style = StyleSheet.create({
     root: {
@@ -13,6 +14,7 @@ const style = StyleSheet.create({
         flexWrap: "wrap",
         alignItems: "center",
         justifyContent: "space-around",
+        minHeight: 100,
     }
 })
 
@@ -26,17 +28,21 @@ async function getAlbum(emby: Emby, id: number, page: number = 1) {
 export function Page({route, navigation}: PropsWithNavigation<"album">) {
     const emby = useAppSelector(state => state.emby?.emby)
     const [data, setData] = useState<EmbyResponse<Media>>()
+    const [loading, setLoading] = useState(true)
     useEffect(() => {
         if (!emby) return
+        setLoading(true)
         getAlbum(emby, Number(route.params.albumId))
         .then(res => {
             setData(res.data)
         })
+        .finally(() => setLoading(false))
     }, [route.params.albumId, emby])
     return (
         <ScrollView showsHorizontalScrollIndicator={false}>
             <View style={style.root}>
                 {data?.Items.map(media => <MediaCard key={media.Id} media={media} />)}
+                {loading ? <Spin /> : null}
             </View>
         </ScrollView>
     )
