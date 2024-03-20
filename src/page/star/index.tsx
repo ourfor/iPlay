1981eebe50351383c2836fbc14a5a7969d3a68cd
ourfor <ourfor@qq.com>
2024-03-20 13:59:47 +0800

    ✨ add vlc internal player

--- src/page/star/index.tsx
+++ src/page/star/index.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from "@view/StatusBar";
 import { SafeAreaView, ScrollView, StyleSheet, View } from "react-native";
+import PlayerView from "@view/Player"
 
 const style = StyleSheet.create({
     page: {
@@ -17,6 +18,10 @@ export function Page() {
                 showsVerticalScrollIndicator={false}
                 style={{flex: 1}}>
                 <View>
+                    <PlayerView style={{width: "100%", aspectRatio: 16/9}}
+                        bgcolor="e0e0e0"
+                        url="https://drive.ourfor.top/iplay/demo.mp4"
+                     />
                 </View>
             </ScrollView>
         </SafeAreaView>
