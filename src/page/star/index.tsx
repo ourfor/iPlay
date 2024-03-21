52652f4f3af1063bfc0a7937de7646911736649c
ourfor <ourfor@qq.com>
2024-03-21 09:11:34 +0800

    🔒️ remove test video url

--- src/page/star/index.tsx
+++ src/page/star/index.tsx
@@ -18,10 +18,6 @@ export function Page() {
                 showsVerticalScrollIndicator={false}
                 style={{flex: 1}}>
                 <View>
-                    {/* <PlayerView style={{width: "100%", aspectRatio: 16/9}}
-                        bgcolor="e0e0e0"
-                        url="http://tkremby.xyz:2095/emby/videos/417367/original.mkv?DeviceId=feed8217-7abd-4d2d-a561-ed21c0b9c30e&MediaSourceId=da699410b4136abee1c65b10cc5d9c9a&PlaySessionId=a595fdb596ea4df3b8a408bcee976ec5&api_key=d639bafb9edd4e0c84f37532c9913f32"
-                     /> */}
                 </View>
             </ScrollView>
         </SafeAreaView>
