9bbf6dbcfe6901b3bde5d7b3f84710a90eec52eb
ourfor <ourfor@qq.com>
2024-03-22 14:40:48 +0800

    👷 add large file upload support

---
+++ tools/upload.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import os
+import requests
+import argparse
+
+parser = argparse.ArgumentParser(description="Upload a file.")
+parser.add_argument("--file", required=True, help="The path to the file to upload.")
+parser.add_argument("--token", required=True, help="The authentication token.")
+parser.add_argument("--url", required=True, help="The URL to upload the file to.")
+
+args = parser.parse_args()
+
+def get_upload_url(toUrl: str, token: str):
+    response = requests.put(toUrl, headers={
+        "Authorization": token,
+        "Content-Length": str(0)
+    })
+    url = response.text
+    return url
+
+chunk_size=5*1024*1024
+
+def upload(file_path: str, url: str, token: str):
+    with open(file_path, 'rb') as file:
+        file.seek(0, os.SEEK_END)
+        size = file.tell()
+        file.seek(0, os.SEEK_SET)
+        print(f"Upload file: {size} url: {url}")
+        link = get_upload_url(url, token)
+        if not link:
+            print("Failed to get upload link")
+            exit(1)
+        while True:
+            start = file.tell()
+            chunk = file.read(chunk_size)
+            end = file.tell() - 1
+            if not chunk:
+                break
+            headers = {
+                "Content-Type": "application/octet-stream",
+                "Content-Length": str(len(chunk)),
+                "Content-Range": f"bytes {start}-{end}/{size}",
+            }
+            print(f"Uploading chunk {start}-{end}/{size}")
+            requests.put(link, data=chunk, headers=headers)
+
+if __name__ == "__main__":
+    upload(args.file, args.url, args.token)
