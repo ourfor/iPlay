b8129f6a20ce1bd27f6f7bf6bfb7cd27f7abbc6b
ourfor <ourfor@qq.com>
2024-03-23 22:25:50 +0800

    💩 add bad code

--- src/page/star/index.tsx
+++ src/page/star/index.tsx
@@ -1,6 +1,7 @@
 import { PropsWithNavigation } from "@global";
+import { IntentModule } from "@helper/native";
 import { StatusBar } from "@view/StatusBar";
-import { SafeAreaView, ScrollView, StyleSheet, View } from "react-native";
+import { Button, SafeAreaView, ScrollView, StyleSheet, View } from "react-native";
 
 const style = StyleSheet.create({
     page: {
@@ -11,7 +12,9 @@ const style = StyleSheet.create({
 
 export function Page(props: PropsWithNavigation<"default">) {
     const { navigation } = props
-
+    const playVideo = () => {
+        IntentModule.playFile("https://drive.endemy.me/iplay/demo.mp4")
+    }
     return (
         <SafeAreaView style={style.page}>
             <StatusBar />
@@ -21,6 +24,7 @@ export function Page(props: PropsWithNavigation<"default">) {
                 showsVerticalScrollIndicator={false}
                 style={{flex: 1}}>
                 <View>
+                    <Button title="播放视频" onPress={playVideo} />
                 </View>
             </ScrollView>
         </SafeAreaView>
