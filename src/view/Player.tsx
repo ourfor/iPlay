73fd56b9e392323d23fd6b4a2373651efab95ef4
ourfor <ourfor@qq.com>
2024-03-25 22:30:32 +0800

    ✨ fix play state

--- src/view/Player.tsx
+++ src/view/Player.tsx
@@ -2,9 +2,17 @@ import React, { forwardRef } from 'react';
 import {ViewProps, requireNativeComponent} from 'react-native';
 const Player = requireNativeComponent('PlayerView');
 
+export enum PlayStateType {
+    PlayEventTypeOnProgress,
+    PlayEventTypeOnPause,
+    PlayEventTypeOnPauseForCache,
+    PlayEventTypeDuration,
+    PlayEventTypeEnd,
+}
+
 export interface PlayerViewProps extends ViewProps {
     title?: string
-    onPlayStateChange?: (state: number) => void
+    onPlayStateChange?: (state: PlayStateType) => void
     url: string
     iconSize?: number
 }
