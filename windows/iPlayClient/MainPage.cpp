b49247f2aef955bdb64c84016b2984b1067996d6
ourfor <ourfor@qq.com>
2024-04-12 22:15:05 +0800

    💄 update windows ui

--- windows/iPlayClient/MainPage.cpp
+++ windows/iPlayClient/MainPage.cpp
@@ -6,15 +6,37 @@
 
 #include "App.h"
 
+#include <winrt/Windows.UI.Core.h>
+#include <winrt/Windows.UI.ViewManagement.h>
+#include <winrt/Windows.ApplicationModel.h>
+#include <winrt/Windows.ApplicationModel.Core.h>
+#include <winrt/Windows.UI.h>
+
 using namespace winrt;
 using namespace xaml;
+using namespace winrt::Windows::UI::ViewManagement;
+using namespace winrt::Windows::UI;
+using namespace winrt::Windows::ApplicationModel::Core;
 
 namespace winrt::iPlayClient::implementation
 {
     MainPage::MainPage()
     {
         InitializeComponent();
+        
         auto app = Application::Current().as<App>();
         ReactRootView().ReactNativeHost(app->Host());
+
+        auto view = CoreApplication::GetCurrentView();
+        auto appTitleBar = view.TitleBar();
+        appTitleBar.ExtendViewIntoTitleBar(true);
+
+        auto appView = ApplicationView::GetForCurrentView();
+        appView.Title(L"Hello World");
+        auto titleBar = appView.TitleBar();
+        titleBar.ForegroundColor(Colors::Transparent());
+        titleBar.BackgroundColor(Colors::Transparent());
+        titleBar.ButtonBackgroundColor(Colors::Transparent());
+        titleBar.ButtonForegroundColor(Colors::Transparent());
     }
 }
