512bf6fb1235e1f8279dfdd36e33f52b267118ab
ourfor <ourfor@qq.com>
2025-01-24 21:23:42 +0800

    🐛 add local drive model json convert

--- android/app/src/main/java/top/ourfor/app/iplayx/model/drive/Drive.java
+++ android/app/src/main/java/top/ourfor/app/iplayx/model/drive/Drive.java
@@ -11,7 +11,8 @@ import top.ourfor.app.iplayx.common.type.ServerType;
         @JsonSubTypes.Type(value = OneDriveModel.class, name = "onedrive"),
         @JsonSubTypes.Type(value = Cloud189Model.class, name = "cloud189"),
         @JsonSubTypes.Type(value = WebDAVModel.class, name = "webdav"),
-        @JsonSubTypes.Type(value = AlistDriveModel.class, name = "alist")
+        @JsonSubTypes.Type(value = AlistDriveModel.class, name = "alist"),
+        @JsonSubTypes.Type(value = LocalDriveModel.class, name = "local"),
 })
 public interface Drive {
     default ServerType getType() {
